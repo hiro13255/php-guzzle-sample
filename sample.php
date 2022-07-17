0f0d28e027c768644a1817ae2da42cf8789e6f37
hiro13255 <kirakira7573@gmail.com>
2022-07-17 17:32:03 +0900

    実際に製品仕様のページのテーブルを取得してコンソールに出力

--- sample.php
+++ sample.php
@@ -1,1 +1,25 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+
+$client = new Client();
+$response = $client->request('GET', 'https://www.sony.jp/ichigan/products/ILCE-7M3/spec.html');
+
+$httpStatusCode = $response->getStatusCode();
+
+if ($httpStatusCode === 200) {
+    $source = $response->getBody()->getContents();
+    $htmlDocument = new Crawler($source);
+
+    $targetElement = $htmlDocument->filter('.s5-specTable>table');
+    $text = $targetElement->text();
+
+    echo $text."\n";
+} else {
+    echo "ステータスコードが正常ではありません。";
+}
+
